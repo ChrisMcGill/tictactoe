81aa69ed695c6ae6bf504df80af1654050e48c4b
Chris <97418708+ChrisMcGill@users.noreply.github.com>
2024-01-02 20:22:35 +0000

    prompts

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// store gameboard as array inside gameboard object
+//store players as objects
+//object to control flow of game
+//logic check for win/loss/tie (eg  [1] == [4] ==[7]   is a win/loss for someone)
+//object to handle display/DOM logic -> function to display array as gameboard with markers
+// function to click on dom to place marker (check if space is available to place marker)
+//allow players to put names
+//start/reset game buttons (declare winners)
+
 const gameboard = [];
